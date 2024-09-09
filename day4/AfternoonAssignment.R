eca503d55b9dbec54babcb0c7cbd004d7d2ae228
Caroline Moore <cmoor143@jh.edu>
2024-09-08 20:40:33 -0400

    Finished Day 4 Afternoon Assignment

--- day4/AfternoonAssignment.R
+++ day4/AfternoonAssignment.R
@@ -51,4 +51,19 @@ paternal_stat <- lm(data = dnm_by_parental_age,
 # The relationship is significant because the p value is < 2e-16
 # Because the p value is so small, it indicates that there is likely a relationship between age of the father and paternal dnms
 
+predicted_paternal_DNMs <- predict(paternal_stat, newdata = data.frame(Father_age = 50.5))
+predicted_paternal_DNMs
 
+# The number of predicted paternal DNMs for a father age 50.5 is 78.69546
+
+ggplot(dnm_by_parental_age, aes(x = n_maternal_dnm)) +
+  geom_histogram(alpha = 0.5, fill = "blue", aes(y = ..density..)) +
+  geom_histogram(aes(x = n_paternal_dnm, y = ..density..), alpha = 0.5, fill = "red") +
+  labs(title = "Distribution of Maternal and Paternal DNMs", x = "Number of DNMs", y = "Density")
+
+t_test_result <- t.test(dnm_by_parental_age$n_maternal_dnm, dnm_by_parental_age$n_paternal_dnm, paired = TRUE)
+t_test_result
+
+# The statistical test I used was a paired t-test as a way to compare the means of the materal DNMs and paternal DNMs; this is because I am comparing between two related groups
+# The p value was < 2.2e-16, meaning there is a significant difference between maternal and paternal DNMs
+# The number of maternally vs. paternally inherited DNMs is highly unlikely to have occurred by chance
